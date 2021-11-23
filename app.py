3a67147743f9faf72568802843b47bbb6e3b8c35
valentinferly14 <75892988+valentinferly14@users.noreply.github.com>
2021-11-23 12:43:44 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import function
+
+daftar_kontak = []
+daftar_kontak.append({
+    "nama" : "Dina",
+    "email" : "dinasalsa@gmail.com",
+    "telepon" : "089546782132"
+})
+
+while True:
+    print("##### Menu #####")
+    print("1. Daftar Kontak")
+    print("2. Tambah Kontak")
+    print("3. Hapus Kontak")
+    print("4. Cari Kontak")
+    print("0. Keluar Program")
+
+    menu = input("Pilih Menu : ")
+
+    if menu == "0":
+        break
+    elif menu == "1":
+        function.display_kontak(daftar_kontak)
+    elif menu == "2":
+        kontak = function.new_kontak()
+        daftar_kontak.append(kontak)
+    elif menu == "3":
+        function.hapus_kontak(daftar_kontak)
+    elif menu == "4":
+        function.cari_kontak(daftar_kontak)
+    else:
+        print("Menu tidak tersedia")
+
+print("Program selesai, see you..")
---
+++ function.py
@@ -0,0 +1,47 @@
+def display_kontak(daftar_kontak):
+    for kontak in daftar_kontak:
+        print("===================")
+        print(f"Nama : {kontak['nama']}")
+        print(f"Email : {kontak['email']}")
+        print(f"Telepon : {kontak['telepon']}")
+        print("===================")
+
+def new_kontak():
+    nama = input("Nama : ")
+    email = input("Email : ")
+    telepon = input("Telepon : ")
+    kontak = {
+        "nama" : nama,
+        "email" : email,
+        "telepon" : telepon
+    }
+    return kontak
+
+def hapus_kontak(daftar_kontak):
+    telepon = input("Nomer telepon yang akan dihapus : ")
+
+    index = -1
+
+    for i in range(0, len(daftar_kontak)):
+        kontak = daftar_kontak[i]
+        if telepon == kontak["telepon"]:
+            index = i
+            break
+
+    if index == -1:
+        print("Data kontak tidak ditemukan")
+    else:
+        del daftar_kontak [index]
+        print("Berhasil menghapus")
+
+def cari_kontak(daftar_kontak):
+    cari_nama = input ("Nama yang di cari : ")
+
+    for kontak in daftar_kontak:
+        nama = kontak["nama"]
+        if nama.lower().find(cari_nama.lower()) != -1:
+            print("===================")
+            print(f"Nama : {kontak['nama']}")
+            print(f"Email : {kontak['email']}")
+            print(f"Telepon : {kontak['telepon']}")
+            print("===================")
